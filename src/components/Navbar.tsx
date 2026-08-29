d38a9dfcf4e8603d365ed23381cbb1cdf9daf6ed
rahul raj <thisisrajtkd18@gmail.com>
2026-08-29 17:30:44 +0530

    correct the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,22 @@
+'use client'
 import React from "react";
 import Image from "next/image";
 import img from "../../public/tour-guide.png"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = ()=>{
+    const path = usePathname(); // it return the path like / , /destination etc
     return(
-        <nav className="bg-white w-full h-15 p-3 flex items-center justify-between text-black">
+        <nav className="bg-white w-full h-15 p-3 flex items-center justify-between text-black fixed  top-0">
             <div className="flex items-center gap-2">
                 <Link href={"/"}> <Image src={img} height={50} alt="tour" width={50}/></Link>
                 <h1 className="font-semibold">Travel Guide</h1>
             </div>
             <div className="flex gap-3 ">
-                <Link href={"/"}> <div className="cursor-pointer">Home</div></Link>
-                <Link href={"/destination"}><div className="cursor-pointer">Destination</div></Link>
-                <Link href={"/contact"}><div className="cursor-pointer">Contact Us</div></Link> 
+                <Link href={"/"}> <div className={ `${path === '/' ? "text-blue-500" : ''} cursor-pointer `}>Home</div></Link>
+                <Link href={"/destination"}><div className={ `${path === '/destination' ? "text-blue-500" : ''} cursor-pointer `}>Destination</div></Link>
+                <Link href={"/contact"}><div className={ `${path === '/contact' ? "text-blue-500" : ''} cursor-pointer `}>Contact Us</div></Link> 
 
 
             </div>
