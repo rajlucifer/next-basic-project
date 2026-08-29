208daa7f9004db10e930663a30ebc5b180e8f89f
rahul raj <thisisrajtkd18@gmail.com>
2026-08-29 17:29:37 +0530

    correct the dynamic routes

--- src/app/destination/[countryName]/page.tsx
+++ src/app/destination/[countryName]/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Image from "next/image";
+import parisImg from  "@/src/assets/paris.jpg"
+import tokyoImg from "@/src/assets/tokyo.webp"
+import nyImg from "@/src/assets/ny.jpg"
 
 type props ={
     params:Promise<{countryName:string}>
@@ -8,8 +12,17 @@ const Country = async({params}:props)=>{
     const {countryName} =  await params ;
     console.log(countryName);
     return(
-        <div>
+        <div className="text-white  mt-20   ">
             Welcome to {countryName} Discover Amazing Sights and culture here
+            {
+                 countryName == "Paris" &&  <div className="  border-2 border-white rounded-3xl object-cover overflow-hidden"><Image src={parisImg} width={500} height={500} alt="paris"></Image></div>
+            }
+            {
+                countryName == "Tokyo" && <div className="  border-2 border-white rounded-3xl object-cover overflow-hidden"><Image src={tokyoImg} width={500} height={500} alt="tokyo"></Image></div>
+            }
+            {
+                countryName == "NewYork" && <div className="  border-2 border-white rounded-3xl object-cover overflow-hidden"><Image src={nyImg} width={500} height={500} alt="new york"></Image></div>
+            }
 
         </div>
     )
